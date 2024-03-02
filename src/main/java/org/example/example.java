485077dc34060dcaa327652a0a160114bd634a99
Ramesh Amale <rameshamale50@gmail.com>
2024-03-02 11:47:35 +0530

    added subtraction method

--- src/main/java/org/example/example.java
+++ src/main/java/org/example/example.java
@@ -5,11 +5,17 @@ public class example {
         System.out.println("Hello world!");
     example ex=new example();
     ex.addition();
+    ex.substrction();
     }
     public void addition(){
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        System.out.println(a+b);}
+    public void substrction(){
+        int a =10;
+        int b=5;
+        System.out.println(a-b);
+
     }
 
 
