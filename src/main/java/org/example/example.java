0ec633da25cd90c31bae112a57088f6f1e5e9a4b
Ramesh Amale <rameshamale50@gmail.com>
2024-03-02 08:15:31 +0530

    addition method added

---
+++ src/main/java/org/example/example.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class example {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    example ex=new example();
+    ex.addition();
+    }
+    public void addition(){
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+
+
+}
